9a4cf8fe2b06f2fe7d291126f070a96a25eaf41f
Katelynn Campbell <klyncamp23@gmail.com>
2017-12-15 01:37:45 +0000

    mins done

--- object.js
+++ object.js
@@ -1,3 +1,28 @@
-document.getElementById("clear").onclick = function (){
+/* global x */
+var disp = document.getElementById("display");
+
+function toScreen(x) {
+   disp.value += x; 
+   if(x==="ce"){
+       disp.value="";
+   }
 }
 
+function answer() {
+    x = disp.value
+    x = eval(x);
+    disp.value = x;
+}
+
+function sqrt() {
+    x = disp.value
+    x = Math.sqrt(x);
+    disp.value = x; 
+}
+
+function backspace() {
+    var num = disp.value;
+    var len = num.length-1;
+    var newNum = num.substring(0,len);
+    disp.value = newNum;
+}
